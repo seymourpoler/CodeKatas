77388ec80027c9ebd79f3761afb2d817b1307ef6
Seymour Poler <seymourpoler@gmail.com>
2011-12-27 15:29:28 +0100

    CodeKata November

---
+++ CodeKatas/CodeKataNovember/CodeKataNovember/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeKataNovember
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Books books;
+            List<int> bks = new List<int>();
+            books = new Books(bks);
+            decimal money = books.GetPrice();
+        }
+    }
+}
