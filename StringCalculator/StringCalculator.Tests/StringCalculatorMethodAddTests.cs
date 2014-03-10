86b70939ff6ea0f8bf465a0188dc4f63957447ca
Luis_Rodriguez_Alejos <luis.rodriguez@adesis.com>
2014-03-10 12:41:52 +0100

    refactoring tests

--- StringCalculator/StringCalculator.Tests/StringCalculatorMethodAddTests.cs
+++ StringCalculator/StringCalculator.Tests/StringCalculatorMethodAddTests.cs
@@ -6,59 +6,59 @@ namespace StringCalculator.Tests
     [TestClass]
     public class StringCalculatorMethodAddTests
     {
+        private StringCalculator _calculator;
+        
+        public StringCalculatorMethodAddTests()
+        {
+            _calculator = new StringCalculator();
+        }
+
         [TestMethod]
         public void When_entry_is_empty_Should_Return_zero()
         {
-            var calculator = new StringCalculator();
-            var result = calculator.Add(string.Empty);
+            var result = _calculator.Add(string.Empty);
             Assert.AreEqual(0, result);
         }
 
         [TestMethod]
         public void When_entry_a_number_Should_Return_the_same_number()
         {
-            var calculator = new StringCalculator();
-            var result = calculator.Add("2");
+            var result = _calculator.Add("2");
             Assert.AreEqual(2, result);
         }
 
         [TestMethod]
         public void When_entry_two_numbers_Should_Return_the_sum()
         {
-            var calculator = new StringCalculator();
-            var result = calculator.Add("1,2");
+            var result = _calculator.Add("1,2");
             Assert.AreEqual(3, result);
         }
 
         [TestMethod]
         public void When_entry_some_numbers_Should_Return_the_sum()
         {
-            var calculator = new StringCalculator();
-            var result = calculator.Add("1,2,3,4,5");
+            var result = _calculator.Add("1,2,3,4,5");
             Assert.AreEqual(15, result);
         }
 
         [TestMethod]
         public void When_entry_some_numbers_separated_throught_some_characters_Should_Return_the_sum()
         {
-            var calculator = new StringCalculator();
-            var result = calculator.Add("1\n2,3");
+            var result = _calculator.Add("1\n2,3");
             Assert.AreEqual(6, result);
         }
 
         [TestMethod]
         public void When_entry_some_numbers_ended_in_a_character_Should_Return_the_sum()
         {
-            var calculator = new StringCalculator();
-            var result = calculator.Add("1,2,3,4,");
+            var result = _calculator.Add("1,2,3,4,");
             Assert.AreEqual(10, result);
         }
 
         [TestMethod]
         public void When_entry_some_numbers_with_different_delimiters_Should_Return_the_sum()
         {
-            var calculator = new StringCalculator();
-            var result = calculator.Add("//;\n1;2");
+            var result = _calculator.Add("//;\n1;2");
             Assert.AreEqual(3, result);
         }
 
@@ -66,10 +66,8 @@ namespace StringCalculator.Tests
         [ExpectedException(typeof(Exception))]
         public void When_entry_some_number_is_negative_Should_Return_an_exception()
         {
-            var calculator = new StringCalculator();
-            var result = calculator.Add("1,2,-3");
+            _calculator.Add("1,2,-3");
             Assert.Fail("Exception expected");
         }
-
     }
 }
