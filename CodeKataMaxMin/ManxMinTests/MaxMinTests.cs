3101561f6e75eafbbbcc8a7b882e09fae11a1977
Luis_Rodriguez_Alejos <luis.rodriguez@adesis.com>
2014-06-02 15:14:13 +0200

    MaxMinManager return max and min from list of intergers

---
+++ CodeKataMaxMin/ManxMinTests/MaxMinTests.cs
@@ -0,0 +1,62 @@
+﻿using MaxMin;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ManxMinTests
+{
+    [TestClass]
+    public class MaxMinTests
+    {
+        private MaxMinManager _maxMin;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _maxMin = new MaxMinManager();
+        }
+
+        [TestMethod]
+        public void MaxMinManager_return_zero_when_input_in_max_is_empty()
+        {
+            var expected = 0;
+            var data = new List<int>();
+
+            var result = _maxMin.Max(data);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void MaxMinManager_return_max_number_when_input_has_one_value()
+        {
+            var expected = 3;
+            var data = new List<int>{3};
+
+            var result = _maxMin.Max(data);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void MaxMinManager_return_min_number_when_input_has_one_value()
+        {
+            var expected = 3;
+            var data = new List<int> { 3 };
+
+            var result = _maxMin.Max(data);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void MaxMinManager_return_zero_when_input_in_min_is_empty()
+        {
+            var expected = 0;
+            var data = new List<int>();
+
+            var result = _maxMin.Min(data);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
