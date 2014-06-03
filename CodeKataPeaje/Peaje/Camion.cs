b4325d69bab96d4a8992bcf4a2397d8061715f96
Luis_Rodriguez_Alejos <luis.rodriguez@adesis.com>
2014-06-03 16:08:03 +0200

    code kata peaje

---
+++ CodeKataPeaje/Peaje/Camion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Peaje
+{
+    public class Camion : IVehiculo
+    {
+        public int Toneladas { get; set; }
+
+        public int PagarPeaje(int impuesto)
+        {
+            return impuesto*Toneladas;
+        }
+
+        public List<IVehiculo> PonerEnEspera(List<IVehiculo> espera)
+        {
+            espera.Add(this);
+            return espera;
+        }
+    }
+}
---
+++ CodeKataPeaje/Peaje/IVehiculo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Peaje
+{
+    public interface IVehiculo
+    {
+        int PagarPeaje(int impuesto);
+        List<IVehiculo> PonerEnEspera(List<IVehiculo>);
+    }
+}
