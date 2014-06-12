45619b529151610af5faa3b81eeec9777ad74888
Luis_Rodriguez_Alejos <luis.rodriguez@adesis.com>
2014-06-12 10:29:28 +0200

    url splitting code kata

---
+++ UrlSplitting/UrlSplitter/Readme.txt
@@ -0,0 +1,12 @@
+﻿URL splitting
+We all know URLs, http://www.google.se is a popular one.
+
+Develop a class that decomposes a given URL into its constituents. In the above example, we would like to get the result
+The protocol: "http"
+The domain name: "www.google.se"
+The path: an empty string in our example.
+Here are some example tests you could write to design this functionality:
+
+"http://some.thing" should give protocol=="http"
+"ftp://a.large.site" should give domain=="a.large.site"
+"http://a.site.with/a-path" should give path=="a-path"
---
+++ UrlSplitting/UrlSplitterTests/UrlSplitTests.cs
@@ -0,0 +1,60 @@
+﻿using UrlSplitter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UrlSplitterTests
+{
+    [TestClass]
+    public class UrlSplitTests
+    {
+        private UrlSplit _urlSplitter;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _urlSplitter = new UrlSplit();
+        }
+        [TestMethod]
+        public void UrlSplit_Split_return_empty_Url_when_url_string_is_empty_string()
+        {
+            var expected = new Url();
+                 
+            var result = _urlSplitter.Split(string.Empty);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void UrlSplit_Split_return_Url_when_url_string_has_http_protocol_and_domain()
+        {
+            var url = "http://www.google.es";
+            var expected = new Url{Protocol = "http", Domain = "www.google.es"};
+
+            var result = _urlSplitter.Split(url);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void UrlSplit_Split_return_Url_when_url_string_has_http_protocol_and_domain_and_path()
+        {
+            var url = "http://www.google.es/path";
+            var expected = new Url { Protocol = "http", Domain = "www.google.es", Path = "path"};
+            var urlSplitter = new UrlSplit();
+
+            var result = _urlSplitter.Split(url);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void UrlSplit_Split_return_Url_when_url_string_has_http_protocol_and_domain_but_without_path()
+        {
+            var url = "http://www.google.es/";
+            var expected = new Url { Protocol = "http", Domain = "www.google.es" };
+
+            var result = _urlSplitter.Split(url);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
