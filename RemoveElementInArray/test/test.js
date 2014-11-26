07acea6f429212ef02f6fd748909bb15b2f8d6bf
Luis_Rodriguez_Alejos <luis.rodriguez@adesis.com>
2014-11-26 20:21:52 +0100

    improve a test

--- RemoveElementInArray/test/test.js
+++ RemoveElementInArray/test/test.js
@@ -5,7 +5,7 @@ describe("Remove a number in an array thought functional way using TDD", functio
   it("Given empty array then return empty array", function(){
     expect(sut.remove(2, [])).to.eql([]);
   });
-  
+
   it("Given array with one element and 0 positions then return the same array", function(){
     expect(sut.remove(2, [2])).to.eql([]);
   });
@@ -13,7 +13,6 @@ describe("Remove a number in an array thought functional way using TDD", functio
     expect(sut.remove(1, [1,2])).to.eql([2]);
   });
   it("Given array and positions then return rotated array without the number", function(){
-    expect(sut.remove(3, [1,2,3,4])).to.eql([1,2,4]);
+    expect(sut.remove(3, [3,2,3,4])).to.eql([2,4]);
   });
-
 });
