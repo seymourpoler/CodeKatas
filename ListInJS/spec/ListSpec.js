f398aed850f92c271c43b49b3c72beb8b5f5e7f7
unknown <luis.rodriguez@Saludonnet-OM.citasonnet.local>
2015-06-05 09:13:28 +0200

    adding sum with filter

--- ListInJS/spec/ListSpec.js
+++ ListInJS/spec/ListSpec.js
@@ -14,13 +14,17 @@ describe("list in javascript using TDD", function(){
 			var numbers = new List();
 			expect(numbers.sum()).to.eql(0);
 		});
-		it("Given list with numbers when call Sum method then returns the sumatory", function(){
+		it("Given list with numbers when call Sum method then returns the sumary", function(){
 			var numbers = new List([1,2,3,4]);
 			expect(numbers.sum()).to.eql(10);
 		});
 		it("Given list with strings when call Sum method then returns only the sum of the numbers ", function(){
 			var numbers = new List([1,'w', {a:1, b:3}, 5, 'rdf']);
 			expect(numbers.sum()).to.eql(6);
+		});
+		it("Given list with strings when call Sum method then returns only the sum of the numbers ", function(){
+			var numbers = new List([1,'w', {a:1, b:3}, 5, {a:6, b:3},'rdf']);
+			expect(numbers.sum(function(elelement){if(element === 'object'){return element.a;}})).to.eql(7);
 		});
 	});
 });
