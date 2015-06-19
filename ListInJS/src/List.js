8bd30bda764163d33f43fd2a56c6f487e34182fc
unknown <luis.rodriguez@Saludonnet-OM.citasonnet.local>
2015-06-19 17:41:35 +0200

    fix tests, but there are red

--- ListInJS/src/List.js
+++ ListInJS/src/List.js
@@ -172,13 +172,14 @@ function List(arrayData){
 		var maxLength = getMaxlength(list);
 		var result = new List();
 		for(var position = 0; position < maxLength; position ++){
-			if(position< data.count() && (position % 2 == 0)){
+			if(position < data.count() && (position % 2 == 0)){
 				result.add(data[position]);
-			}else if(position < list.count() && (position % 2 != 0))
+			}else if(position < list.count() && (position % 2 != 0)){
 				result.add(list.getItem(position))
 			}
 		}
 		return result;
+		
 		
 		function getMaxlength(list){
 			if(data.count() <= list.count()){
