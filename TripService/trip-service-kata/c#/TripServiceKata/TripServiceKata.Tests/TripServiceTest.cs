643c434badce36aaa855d9c570683cc507369d21
Seymour Poler <seymourpoler@gmail.com>
2015-10-23 21:28:20 +0200

    first test exploratory

---
+++ TripService/trip-service-kata/c#/TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
@@ -0,0 +1,19 @@
+﻿namespace TripServiceKata.Tests
+{
+	using NUnit.Framework;
+	using TripServiceKata.Trip;
+	using TripServiceKata.User;
+
+	[TestFixture]
+    public class TripServiceTest
+    {
+		[Test]
+		public void pp()
+		{
+			var service = new TripService ();
+			var result = service.GetTripsByUser (new User ());
+
+			Assert.IsNotNull (result);
+		}
+    }
+}
