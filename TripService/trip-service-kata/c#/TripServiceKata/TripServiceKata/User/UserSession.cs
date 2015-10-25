ec3aeb0e070daa7282e5b7453f0c6cae06ee19de
Seymour Poler <seymourpoler@gmail.com>
2015-10-25 09:28:08 +0100

    make public constructor in userSession

--- TripService/trip-service-kata/c#/TripServiceKata/TripServiceKata/User/UserSession.cs
+++ TripService/trip-service-kata/c#/TripServiceKata/TripServiceKata/User/UserSession.cs
@@ -6,7 +6,7 @@ namespace TripServiceKata.User
     {
         private static readonly UserSession userSession = new UserSession();
 
-        private UserSession() { }
+        public UserSession() { }
 
         public static UserSession GetInstance()
         {
