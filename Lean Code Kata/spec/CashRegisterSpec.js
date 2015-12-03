75690e8b6a9a216b51b4b9b531c0faf37fef7d50
Seymour Poler <seymourpoler@gmail.com>
2015-12-03 16:36:16 +0100

    refactor: magic words as constants

--- Lean Code Kata/spec/CashRegisterSpec.js
+++ Lean Code Kata/spec/CashRegisterSpec.js
@@ -1,45 +1,77 @@
 describe("Cash Register", function() {
   var cashRegister;
+  var CHERRY = 'cherry';
+  var BANNANA = 'bannana';
+  var PLANTANO = 'platano';
+  var APPLE = 'apple';
 
   beforeEach(function() {
     cashRegister = new CashRegister();
   });
+
   it("the price for a bannana", function(){
-    var billResult = cashRegister.with('bannana').getTheBill();
+    var billResult = cashRegister.with(BANNANA).getTheBill();
+    
     expect(billResult.totalPrice).toEqual(100);
   });
   it("the price for a apple", function(){
-    var billResult = cashRegister.with('apple').getTheBill();
+    var billResult = cashRegister.with(APPLE).getTheBill();
+    
     expect(billResult.totalPrice).toEqual(75);
   });
   it("the price for a cherry", function(){
-    var billResult = cashRegister.with('cherry').getTheBill();
+    var billResult = cashRegister.with(CHERRY).getTheBill();
+    
     expect(billResult.totalPrice).toEqual(90);
   });
   it("the price for cherry and apple", function(){
-    var billResult = cashRegister.with('cherry').with('apple').getTheBill();
+    var billResult = cashRegister.with(CHERRY)
+                                  .with(APPLE)
+                                  .getTheBill();
+    
     expect(billResult.totalPrice).toEqual(165);
   });
   it("the price for apple and bannana", function(){
-    var billResult = cashRegister.with('apple').with('bannana').getTheBill();
+    var billResult = cashRegister.with(APPLE)
+                                  .with(BANNANA)
+                                  .getTheBill();
+    
     expect(billResult.totalPrice).toEqual(175);
   });
   it("the price for apple, bannana and cherry", function(){
-    var billResult = cashRegister.with('apple').with('bannana').with('cherry').getTheBill();
+    var billResult = cashRegister.with(APPLE)
+                                  .with(BANNANA)
+                                  .with(CHERRY)
+                                  .getTheBill();
+    
     expect(billResult.totalPrice).toEqual(265);
   });
   it("the price for apple, bannana and cherry", function(){
-    var billResult = cashRegister.with('apple').with('bannana').with('cherry').getTheBill();
+    var billResult = cashRegister.with(APPLE)
+                                  .with(BANNANA)
+                                  .with(CHERRY)
+                                  .getTheBill();
+    
     expect(billResult.totalPrice).toEqual(265);
   });
   it("second cherry have price", function(){
-    var billResult = cashRegister.with('cherry')
-                                  .with('cherry')
+    var billResult = cashRegister.with(CHERRY)
+                                  .with(CHERRY)
                                   .getTheBill();
+    
     expect(billResult.totalPrice).toEqual(135);
   });
   it("the price for a type of bannana, is platano", function(){
-    var billResult = cashRegister.with('platano').getTheBill();
+    var billResult = cashRegister.with(PLANTANO).getTheBill();
     expect(billResult.totalPrice).toEqual(105);
+  });
+  it("3 cherrys plus apple", function(){
+    var billResult = cashRegister.with(CHERRY)
+                                  .with(CHERRY)
+                                  .with(CHERRY)
+                                  .with(APPLE)
+                                  .getTheBill();
+    
+    expect(billResult.totalPrice).toEqual(300);
   });
 });
