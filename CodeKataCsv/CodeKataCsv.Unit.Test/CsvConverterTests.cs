1439edb85e554fa6fc95db39781d0b3669d59f46
Luis Rodriguez Alejos <lralejos@RIADEV.local>
2018-05-22 14:26:43 +0100

    adding

---
+++ CodeKataCsv/CodeKataCsv.Unit.Test/CsvConverterTests.cs
@@ -0,0 +1,49 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CodeKataCsv.Unit.Test
+{
+    public class CsvConverterTests
+    {
+        [Fact]
+        public void ReturnsAnEmtpyListWhenTheListIsEmpty()
+        {
+            var list = new List<User>();
+            var converter = new CsvConverter();
+
+            var result = converter.Convert(list);
+
+            result.ShouldBe(new string[] { });
+        }
+
+        [Fact]
+        public void ReturnsAnEmtpyListWhenTheListIsNull()
+        {
+            var converter = new CsvConverter();
+
+            var result = converter.Convert((List<User>)null);
+
+            result.ShouldBe(new string[] { });
+        }
+
+        [Fact]
+        public void ReturnsCsvWhenTheListHasAnEmptyElement()
+        {
+            var converter = new CsvConverter();
+
+            var result = converter.Convert(new List<User> { new User { } });
+
+            result.ShouldBe(new string[] { ";;;" });
+        }
+
+        class User
+        {
+            public string Email { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Age { get; set; }
+        }
+    }
+}
---
+++ CodeKataCsv/CodeKataCsv/CsvConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeKataCsv
+{
+    public class CsvConverter
+    {
+        public IReadOnlyCollection<string> Convert<T>(List<T> list) 
+            where T : class
+        {
+            if(list == null || list.Count == 0)
+            { 
+                return new string[] { };
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
