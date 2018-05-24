ce8d0ec6a34561ca179eee63fdfdb5e7671d96b5
Luis Gabriel Rodriguez Alejos <lralejos@riafinancial.com>
2018-05-24 20:05:03 +0200

    green
    	- ReturnsCsvWhenTheListHasSomeElements
    	- ReturnsCsvWhenTheListHasAllElements

--- CodeKataCsv/CodeKataCsv.Unit.Test/CsvConverterTests.cs
+++ CodeKataCsv/CodeKataCsv.Unit.Test/CsvConverterTests.cs
@@ -1,23 +1,12 @@
 ﻿using Shouldly;
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace CodeKataCsv.Unit.Test
 {
     public class CsvConverterTests
     {
-        [Fact]
-        public void ReturnsAnEmtpyListWhenTheListIsEmpty()
-        {
-            var list = new List<User>();
-            var converter = new CsvConverter();
-
-            var result = converter.Convert(list);
-
-            result.ShouldBe(new string[] { });
-        }
-
         [Fact]
         public void ReturnsAnEmtpyListWhenTheListIsNull()
         {
@@ -29,16 +18,47 @@ namespace CodeKataCsv.Unit.Test
         }
 
         [Fact]
+        public void ReturnsAnEmtpyListWhenTheListIsEmpty()
+        {
+            var list = new List<User>();
+            var converter = new CsvConverter();
+
+            var result = converter.Convert(list);
+
+            result.ShouldBe(new[] { "Email;FirstName;LastName;Age" });
+        }
+
+        [Fact]
         public void ReturnsCsvWhenTheListHasAnEmptyElement()
         {
             var converter = new CsvConverter();
 
             var result = converter.Convert(new List<User> { new User { } });
 
-            result.ShouldBe(new string[] { ";;;" });
+            result.ShouldBe(new[] { "Email;FirstName;LastName;Age", ";;;0" });
         }
 
-        class User
+        [Fact]
+        public void ReturnsCsvWhenTheListHasSomeElements()
+        {
+            var converter = new CsvConverter();
+
+            var result = converter.Convert(new List<User> { new User {Email="a@a.es" } });
+
+            result[1].ShouldBe("a@a.es;;;0");
+        }
+
+        [Fact]
+        public void ReturnsCsvWhenTheListHasAllElements()
+        {
+            var converter = new CsvConverter();
+
+            var result = converter.Convert(new List<User> { new User { FirstName = "First", LastName = "Last", Email = "a@a.es", Age = 24 } });
+
+            result[1].ShouldBe("a@a.es;First;Last;24");
+        }
+
+        public class User
         {
             public string Email { get; set; }
             public string FirstName { get; set; }
--- CodeKataCsv/CodeKataCsv/CsvConverter.cs
+++ CodeKataCsv/CodeKataCsv/CsvConverter.cs
@@ -1,18 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace CodeKataCsv
 {
     public class CsvConverter
     {
-        public IReadOnlyCollection<string> Convert<T>(List<T> list) 
+        public IList<string> Convert<T>(List<T> list) 
             where T : class
         {
-            if(list == null || list.Count == 0)
-            { 
-                return new string[] { };
+            if(list == null)
+            {
+                return new List<string>();
             }
-            throw new NotImplementedException();
+            var result = new List<string>
+            {
+                GetHeader<T>(),
+            };
+            result.AddRange(GetBody(list));
+            return result;
+        }
+
+        private IEnumerable<string> GetBody<T>(List<T> list)
+        {
+            foreach (var item in list)
+            {
+                yield return GetValues(item);
+            }
+        }
+
+        private string GetHeader<T>() 
+        {
+            PropertyInfo[] properties = typeof(T).GetProperties();
+            var headers = new List<string>();
+            foreach(var property in properties)
+            {
+                headers.Add(property.Name);
+            }
+            return String.Join(";", headers.ToArray());
+        }
+
+        private string GetValues<T>(T item)
+        {
+            PropertyInfo[] properties = typeof(T).GetProperties();
+            var values = new List<string>();
+            foreach (var property in properties)
+            {
+                values.Add(GetValue(item, property));
+            }
+            return String.Join(";", values.ToArray());
+        }
+
+        private string GetValue<T>(T item, PropertyInfo property)
+        {
+            return System.Convert.ToString(property.GetValue(item));
         }
     }
 }
