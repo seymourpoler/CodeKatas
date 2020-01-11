2e7f3f4584fcd53999d1a9780035397f5aa58aaf
seymourpoler <seymourpoler@gmail.com>
2020-01-11 16:14:00 +0100

    setup code kata fizzbuzz in dotnet core

---
+++ FizzBuzz/dotNetCore/FizzBuzz/FizzBuzz/FizBuzzShould.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace FizzBuzz
+{
+    public class FizBuzzShould
+    {
+        [Fact]
+        public void Works()
+        {
+            
+            Assert.True(true);
+        }
+    }
+}
