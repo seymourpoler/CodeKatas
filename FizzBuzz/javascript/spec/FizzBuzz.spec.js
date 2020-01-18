d04833e1f414971341df58e51382f13bd0106e78
seymourpoler <seymourpoler@gmail.com>
2020-01-18 16:19:19 +0100

    resolve code kata

--- FizzBuzz/javascript/spec/FizzBuzz.spec.js
+++ FizzBuzz/javascript/spec/FizzBuzz.spec.js
@@ -1,7 +1,50 @@
 const fizzBuzz = require('../src/FizzBuzz');
+//import {fizzBuzz} from '../src/FizzBuzz';
 
 describe('fizz buzz kata', () => {
     it('works', () => {
         expect(true).toBe(true);
+    });
+
+    it('returns empty when is null', ()=>{
+        const result = fizzBuzz(null);
+
+        expect('').toBe(result);
+    });
+
+    it('returns empty when is undefined', () => {
+        const result = fizzBuzz(undefined);
+
+        expect('').toBe(result);
+    });
+
+    it('returns empty when is under one', () => {
+        const result = fizzBuzz(-1);
+
+        expect('').toBe(result);
+    });
+    
+    it('returns empty when is zero', () => {
+        const result = fizzBuzz(0);
+
+        expect('').toBe(result);
+    });
+    
+    it('returns number when is not divided by three and not five', () =>{
+        const result = fizzBuzz(4);
+
+        expect(4).toBe(result);
+    });
+
+    it('returns buzz when is divided by five', () => {
+        const result = fizzBuzz(5);
+
+        expect('buzz').toBe(result);
+    })
+
+    it('return fizzbuzz when is divided by thre and five', ()=>{
+        const result = fizzBuzz(15);
+
+        expect('fizzbuzz').toBe(result);
     });
 });
--- FizzBuzz/javascript/src/FizzBuzz.js
+++ FizzBuzz/javascript/src/FizzBuzz.js
@@ -1,5 +1,36 @@
-function fizzBuzz(){
-    throw new Error('not implemented');
+function fizzBuzz(number){
+    if(IsNotNumber(number)){
+        return '';
+    }
+    
+    if(number <= 0){
+        return '';
+    }
+    
+    if(isDividedByThree(number) && isDividedByFive(number)){
+        return 'fizzbuzz';
+    }
+    if(isDividedByThree(number)){
+        return 'fizz';
+    }
+    if(isDividedByFive(number)){
+        return 'buzz';
+    }
+
+    return number;
+
+    function IsNotNumber(aNumber){
+        return aNumber === undefined || aNumber === null;
+    }
+
+    function isDividedByThree(aNumber){
+        return aNumber % 3 == 0;
+    }
+
+    function isDividedByFive(aNumber){
+        return aNumber % 5 == 0;
+    }
 }
 
 module.exports = fizzBuzz;
+//export function fizzBuzz;
